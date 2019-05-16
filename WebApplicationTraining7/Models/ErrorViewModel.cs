baa9ee43236e2baf64bb054bb3a8b5600cbd7149
user@az7tr.onmicrosoft.com <user@az7tr.onmicrosoft.com>
2019-05-16 12:36:34 +0200

    add

---
+++ WebApplicationTraining7/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationTraining7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
